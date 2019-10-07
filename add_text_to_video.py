7cfe4750d276b1b60737b8fa77b5ce93b43aab36
standemdem <37410638+standemdem@users.noreply.github.com>
2019-10-07 15:52:21 +0200

    how to use set()

--- add_text_to_video.py
+++ add_text_to_video.py
@@ -8,7 +8,7 @@ cap = cv2.VideoCapture(0)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fps = int(cap.get(cv2.CAP_PROP_FPS))
-
+a
 # Set some values for width (3)
 cap.set(3, 640)
 # Set some values for height (4)
