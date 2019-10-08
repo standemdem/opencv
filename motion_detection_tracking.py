99dcecb078d5df76dacebcf4f28e3e8e4b2cf649
standemdem <37410638+standemdem@users.noreply.github.com>
2019-10-08 11:05:36 +0200

    add comments to the code for later review

--- motion_detection_tracking.py
+++ motion_detection_tracking.py
@@ -7,32 +7,32 @@ ret, frame1 = cap.read()
 ret, frame2 = cap.read()
 
 while cap.isOpened():
-
     # calculating the absolute difference between first and second frames
     diff = cv2.absdiff(frame1, frame2)
-
     # easier to find contours when it's gray
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-
+    # Even more easy if you blur the image using a Gaussian algorythm
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    # Apply binary threshold to blurred image
     _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
-
+    # Increases the quality of contouring (need further studies)
     dilated = cv2.dilate(thresh, None, iterations=3)
-
+    # find the contours on the dilated image
     _, contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    #cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
-
-
+    # Draw the contours
     for contour in contours:
         (x, y, w, h) = cv2.boundingRect(contour)
-
-        if cv2.contourArea(contour)  < 9500:
+        if cv2.contourArea(contour) < 2000:
             continue
-        cv2.rectangle(frame1, (x,y), (x+w, y+h), (0, 255, 0), 2)
-        text = 'Status: {}'.format('movement')
-        cv2.putText(frame1, text, (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
-                    1, (0, 0, 255), 3 )
+        cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(frame1, 'status: {}'.format('Movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
+                    1, (0, 0, 255), 3)
+
+    # For very fine contouring (old version)
+    # cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
+
+    # Display camera
     cv2.imshow('test', frame1)
 
     frame1 = frame2
